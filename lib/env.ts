864b69024a5668a870fef269125bb783a9e6ec61
salif <tanksalif@gmail.com>
2025-07-17 20:09:02 -0400

    add env config loader

---
+++ lib/env.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env.local") });
+
+export const env = {
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  PINECONE_API_KEY: process.env.PINECONE_API_KEY,
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  VAPI_API_KEY: process.env.VAPI_API_KEY,
+  VAPI_ASSISTANT_ID: process.env.VAPI_ASSISTANT_ID,
+};
